c26c5eea150adf75c894a59e2df73a39cc8b5571
GitSooth <a15823@alunos.ipca.pt>
2022-06-07 21:25:48 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Intro from "./components/intro/intro";
+import About from "./components/about/about";
+
+const App = () => {
+  return (
+    <div>
+      <Intro />
+      <About />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/about/about.jsx
@@ -0,0 +1,20 @@
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*"
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="a-right">RIGHT</div>
+    </div>
+  );
+};
+
+export default About;
