17dd133f59b8c0594231ea96f60df7c2c4b7f144
GitSooth <a15823@alunos.ipca.pt>
2022-06-14 19:13:00 +0100

    Initial

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,14 @@
+import "./productList.css";
+
+const ProductList = () => {
+  return (
+    <div className="p1">
+      <div className="pl-texts">
+        <h1 className="pl-title">Random text</h1>
+        <p className="pl-desc">Some other random text</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
